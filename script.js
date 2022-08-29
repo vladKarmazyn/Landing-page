38f3cb7fb5cde649f2d145b104395a435112c7f2
Vladyslav Karmazyn <karmazynnnn@gmail.com>
2022-08-29 21:03:22 +0300

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+const mobileButton = document.querySelector('#btn_burger');
+const mobileNav = document.querySelector('#burger_menu');
+
+mobileButton.addEventListener ('click' , function() {
+mobileNav.classList.toggle('burger_active')
+
+});
